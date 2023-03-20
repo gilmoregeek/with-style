685653d80c534500fb1c961267766fba8e399035
sabrina.anke <sabrina.anke@stud.hs-hannover.de>
2023-03-20 18:51:51 +0100

    Add mock data to startpage

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar</a>
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>HEADLINE</h1>
+        <div className="teaser-wrapper">
+          <div className="teaser-tile tile-1">
+            <h2>HEADLINE</h2>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+          </div>
+          <div className="teaser-tile tile-2">
+            <h2>HEADLINE</h2>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+          </div>
+          <div className="teaser-tile tile-3">
+            <h2>HEADLINE</h2>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+          </div>
+        </div>
       </header>
+      <footer className="footer">
+        some footer infos
+      </footer>
     </div>
   );
 }
